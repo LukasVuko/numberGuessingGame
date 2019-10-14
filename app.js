cef3e466488a2d525af68b726aa2cf388a55acb4
Lukas Vukovich <lukas.vukovich@gmail.com>
2019-10-14 12:06:22 -0400

    Added random number functionality and gameover triggers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Game Values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNumber(min, max),
   guessesLeft = 3;
 
 const game = document.querySelector('#game'),
@@ -15,33 +15,35 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+
+game.addEventListener('mousedown', function(e) {
+  if (e.target.className === 'play-again') {
+    window.location.reload();
+  }
+});
+
 // Listen for guess
 guessBtn.addEventListener('click', function() {
   let guess = parseInt(guessInput.value);
-  // Validate
+  console.log(guess);
   if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  } else if (guess === winningNum) {
+    gameOver(true, `${winningNum} is correct! You won!`);
   } else {
-    if (guess === winningNum) {
-      guessInput.disabled = true;
-      guessInput.style.borderColor = 'green';
-      setMessage(`${winningNum} is correct! You won!`, 'green');
+    guessesLeft -= 1;
+    if (guessesLeft === 0) {
+      gameOver(
+        false,
+        `Game over. You lost! The correct number was ${winningNum}.`
+      );
     } else {
-      guessesLeft -= 1;
-      if (guessesLeft === 0) {
-        setMessage(
-          `Game over. You lost. The correct number was ${winningNum}.`,
-          'red'
-        );
-        guessBtn.value = 'Reset';
-        guessInput.disabled = true;
-      } else {
-        setMessage(
-          `${guess} was incorrect. You have ${guessesLeft} guess left.`,
-          'red'
-        );
-        guessInput.value = '';
-      }
+      setMessage(
+        `${guess} was incorrect. You have ${guessesLeft} guess left.`,
+        'red'
+      );
+      guessInput.value = '';
     }
   }
 });
@@ -49,4 +51,22 @@ guessBtn.addEventListener('click', function() {
 function setMessage(msg, color) {
   message.textContent = msg;
   message.style.color = color;
+}
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = 'green') : (color = 'red');
+
+  setMessage(msg, color);
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+
+  guessBtn.value = 'Play again';
+  guessBtn.className += 'play-again';
+}
+
+function getRandomNumber(min, max) {
+  let num = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(num);
+  return num;
 }
